122dc4ceb871c4fa921883618e470028787b1b7a
francostan <francostan98@hotmail.com>
2024-01-05 16:57:32 -0300

    new schema

--- prisma/migrations/20240105195617_finish2/migration.sql
+++ prisma/migrations/20240105195617_finish2/migration.sql
@@ -5,6 +5,8 @@ CREATE TABLE "Note" (
     "description" TEXT,
     "tags" TEXT[],
     "email" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL,
+    "categories" TEXT[],
 
     CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
 );
